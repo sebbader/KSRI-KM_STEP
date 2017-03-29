0c79cd837c6d1ed08482883000161de2b21399fd
Sebastian Bader <sebastian.bader@kit.edu>
2017-03-29 18:10:12 +0200

    introducing Semantic Annotator project

--- src/main/java/edu/kit/aifb/step/web/WebEngine.java
+++ src/main/java/edu/kit/aifb/step/web/WebEngine.java
@@ -7,16 +7,30 @@ import javax.ws.rs.ApplicationPath;
 import javax.ws.rs.core.Application;
 
 import edu.kit.aifb.step.sensor.web.AdministrationShellWebAPI;
+import io.swagger.jaxrs.config.BeanConfig;
 
 @ApplicationPath(value = "/wrapper")
 public class WebEngine extends Application {
-	
+
+//	public WebEngine() {
+//		BeanConfig beanConfig = new BeanConfig();
+//		beanConfig.setVersion("1.0.0");
+//		beanConfig.setBasePath("/api");
+//		beanConfig.setResourcePackage("org.jazzteam");
+//		beanConfig.setScan(true);
+//	}
+
+
 	@Override
 	public Set<Class<?>> getClasses() {
-        Set<Class<?>> s = new HashSet<Class<?>>();
-        s.add(AdministrationShellWebAPI.class);
-        return s;
-    }
-	
+		Set<Class<?>> resources = new HashSet<Class<?>>();
+		resources.add(AdministrationShellWebAPI.class);
+
+//		resources.add(io.swagger.jaxrs.listing.ApiListingResource.class);
+//		resources.add(io.swagger.jaxrs.listing.SwaggerSerializers.class);
+
+		return resources;
+	}
+
 
 }

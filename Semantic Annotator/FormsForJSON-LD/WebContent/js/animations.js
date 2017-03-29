53834b4b9108d3d2f4d9950e09add94b639e1b87
William1Blake <Nazgul124750@yandex.ru>
2017-03-29 18:21:07 +0200

    Add files via upload

---
+++ Semantic Annotator/FormsForJSON-LD/WebContent/js/animations.js
@@ -0,0 +1,16 @@
+$('#main-page').hover(
+  function(){ $("#home").addClass('animated bounce') },
+  function(){ $("#home").removeClass('animated bounce') }
+  );
+$('#ueber-mich').hover(
+  function(){ $("#book").addClass('animated tada') },
+  function(){ $("#book").removeClass('animated tada') }
+  );
+$('#leistungen').hover(
+  function(){ $("#star").addClass('fa-spin-ones') },
+  function(){ $("#star").removeClass('fa-spin-ones') }
+  );
+$('#artikel').hover(
+  function(){ $("#comments").addClass('animated jello') },
+  function(){ $("#comments").removeClass('animated jello') }
+  );

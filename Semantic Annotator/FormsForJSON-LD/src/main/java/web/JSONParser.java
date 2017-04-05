748658de4fc10e8b6828fb3d0cd14659fbbb4ed8
William1Blake <Nazgul124750@yandex.ru>
2017-04-05 23:30:00 +0200

    Small update

--- Semantic Annotator/FormsForJSON-LD/src/main/java/web/JSONParser.java
+++ Semantic Annotator/FormsForJSON-LD/src/main/java/web/JSONParser.java
@@ -1,21 +1,13 @@
 package main.java.web;
 
-import java.io.InputStream;
-import java.nio.charset.StandardCharsets;
-import java.io.ByteArrayInputStream;
 import java.io.IOException;
 import java.util.HashMap;
 import java.util.Iterator;
 import java.util.Map;
 
-import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
-import javax.ws.rs.Produces;
-import javax.ws.rs.core.MediaType;
-
-import org.json.JSONObject;
 
 import com.github.jsonldjava.core.JsonLdError;
 import com.github.jsonldjava.core.JsonLdOptions;
@@ -34,24 +26,15 @@ public class JSONParser {
 //	@Produces("application/json-ld")
 //	@Consumes(MediaType.TEXT_PLAIN)
 	public String parseJSONLD(String text) throws IOException, JsonLdError {
-		Object jsonObject = JsonUtils.fromString(text);
-		// Create a context JSON map containing prefixes and definitions
-		 Map<Object, Object> context = new HashMap<Object, Object>();
-		 context.put("@vocab", "http://schema.org");
-		 
-		 /*context.put(2, "point");
-		 Iterator<?> keys = jsonObject.keys();
-		 while( keys.hasNext() ) {
-			    String key = (String)keys.next();
-			    if ( jsonObject.get(key) instanceof JSONObject ) {
-			    	 context.put(jsonObject.get(key), jsonObject.get(key));
-			    }
-			}*/
 		
+		Object jsonObject = JsonUtils.fromString(text);
+		//Object jsonObject = JsonLdProcessor.fromRDF(text);
+		// Create a context JSON map containing prefixes and definitions
+		Map<Object, Object> context = new HashMap<Object, Object>();
+		context.put("@vocab", "http://schema.org");
 		// Customise context...
 		// Create an instance of JsonLdOptions with the standard JSON-LD options
 		JsonLdOptions options = new JsonLdOptions();
-		
 		// Customise options...
 		// Call whichever JSONLD function you want! (e.g. compact)
 		Map<String, Object> compact = JsonLdProcessor.compact(jsonObject, context, options);
